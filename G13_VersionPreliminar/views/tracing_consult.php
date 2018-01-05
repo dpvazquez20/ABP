58d6d916776970b02533c7f48ee4c83918a3b08a
SergioESEI <saveiga@esei.uvigo.es>
2018-01-05 07:14:50 +0100

    Seguimiento desde deportista

--- G13_VersionPreliminar/views/tracing_consult.php
+++ G13_VersionPreliminar/views/tracing_consult.php
@@ -43,12 +43,16 @@ class TracingConsult
                             <a href='tracing_controller.php?sesionId=<?php echo $this->data1[0]['sesionId']; ?>&action=<?php echo $strings['previousTable']; ?>' class="btn btn-md btn-primary" id="newButton"><span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true"></span> <?php echo $strings['previous']; ?>  </a>
                             <a href='tracing_controller.php?sesionId=<?php echo $this->data1[0]['sesionId']; ?>&action=<?php echo $strings['nextTable']; ?>' class="btn btn-md btn-primary" id="newButton"> <?php echo $strings['next']; ?> <span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true"></span> </a>
                         </div>
+
+                        <div class="col-md-12" id="new">
+                            <a href='tracing_controller.php?sesionId=<?php echo $this->data1[0]['sesionId']; ?>&action=<?php echo $strings['startTime']; ?>' class="btn btn-md btn-warning" id="newButton"> <?php echo $strings['startTime']; ?> <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span> </a>
+                        </div>
                     <?php
                     }
                     ?>
 
                         <?php
-                            $titles = array('imagen', 'nombre', 'series', 'repeticiones', 'duracion', 'descanso','comentario','completado');
+                            $titles = array('imagen', 'nombre', 'series', 'repeticiones', 'duracion', 'descanso','completado');
 
                             if ($_SESSION['userType'] == $strings['coach'])
                             {
@@ -57,17 +61,51 @@ class TracingConsult
                                 generateViewTracingTitle($this->data1,false);
                             }
 
+
+                            $coment = '';
+
+                            if(isset($this->data1[0]['comentario']))
+                            {
+                                $coment = $this->data1[0]['comentario'];
+                            }
+
                             if($this->locked)
                             {
                                 generateViewTracingCoach2($this->data2, 'tracing', $titles);
+
+                        ?>
+                            <div class="col-md-12" id="new">
+                                <textarea name="comment" style="width:90%; margin-bottom: 10px" rows="10" readonly><?php echo $coment; ?></textarea>
+                            </div>
+                        <?php
                             }else{
                                 generateViewTracingSportsman2($this->data2, 'tracing', $titles,$strings['completeLine']);
-                                       
+
+                                if($coment == '')
+                                {
+                                    $coment = $strings['writeComments'];
+                                }
                         ?>
 
                         <div class="col-md-12" id="new">
                             <a href='tracing_controller.php?sesionId=<?php echo $this->data1[0]['sesionId']; ?>&action=<?php echo $strings['completeTable']; ?>' class="btn btn-md btn-warning" id="newButton"> <?php echo $strings['toComplete']; ?> <span class="glyphicon glyphicon glyphicon-send" aria-hidden="true"></span> </a>
                         </div>
+
+                        <form class="form-horizontal" action='../controllers/tracing_controller.php?sesionId=<?php echo $this->data1[0]['sesionId']; ?>&' method='post'>
+                            <div class="form-group">
+
+                                <div class="col-md-12" id="new">
+                                    <textarea name="comment" style="width:90%; margin-bottom: 10px" rows="10"><?php echo $coment; ?></textarea>
+                                </div>
+
+                                <div class="col-md-offset-2 col-md-8" id="addEditButtons">
+                                    <button type="submit" class="btn btn-md btn-success" name="action" value="<?php echo $strings['comment']; ?>"> <?php echo $strings['Accept']; ?> </button>
+                                    <a href="../controllers/tracing_controller.php" class="btn btn-md btn-danger"> <?php echo $strings['Cancel']; ?> </a>
+                                    <button type="reset" class="btn btn-md btn-default"> <?php echo $strings['Reset']; ?> </button>
+                                </div>
+
+                            </div>
+                        </form>
 
                         <?php
                             }
