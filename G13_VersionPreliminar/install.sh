714194c49f6e4bc1a481aeaf81a708f67b44f9bc
dpvazquez20 <dpvazquez@esei.uvigo.es>
2018-01-14 11:42:55 +0100

    Update install.sh

--- G13_VersionPreliminar/install.sh
+++ G13_VersionPreliminar/install.sh
@@ -1,5 +1,7 @@
 #Permissions installer
 
+#chown nobody:nogroup -R /var/www/html/G13_VersionFinal
+#chmod -R 777 /var/www/html/G13_VersionFinal
 chown nobody:nogroup -R /var/www/html/G13_VersionPreliminar
 chmod -R 777 /var/www/html/G13_VersionPreliminar
 #chown nobody:nogroup -R /var/www/html/ActiFit
